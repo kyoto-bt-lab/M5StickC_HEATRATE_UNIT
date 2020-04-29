36a37b57dcd88436011856c3f80a27bc16602a3f
kyoto-bt-lab <>
2020-04-29 22:51:40 +0900

    first

---
+++ M5StickC_HEATRATE_UNIT.ino
@@ -0,0 +1,119 @@
+#include <M5StickC.h>
+#include <Wire.h>
+#include "MAX30100_PulseOximeter.h"
+#include "xbm.h" //my bitmap
+
+#define REPORTING_PERIOD_MS     1000
+// PulseOximeter is the higher level interface to the sensor
+// it offers:
+//  * beat detection reporting
+//  * heart rate calculation
+//  * SpO2 (oxidation level) calculation
+PulseOximeter pox;
+
+uint32_t tsLastReport = 0;
+unsigned int lastBeat = 0;
+bool isHeatBeatAvailable = false;
+uint16_t mainColor;
+uint16_t goodColor;
+
+// Callback (registered below) fired when a pulse is detected
+void onBeatDetected()
+{
+    Serial.println("B:1");
+    M5.Lcd.drawXBitmap(10, 5, hb2_bmp, hb2_bmp_width, hb2_bmp_height, mainColor);
+    isHeatBeatAvailable = true;
+}
+
+void printHeartBeat(float x){
+    M5.Lcd.fillRect(70, 15, 100, 100,  BLACK);
+    if(60 <= x && x <= 100){
+        M5.Lcd.setTextColor(goodColor);
+    }else{
+        M5.Lcd.setTextColor(mainColor);
+    }
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setCursor(70,15);
+    M5.Lcd.printf("%03.0f", x);
+}
+void printSpO2(int x){
+    M5.Lcd.fillRect(12, 70, 100, 100,  BLACK);
+    M5.Lcd.setTextColor(mainColor);
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setCursor(12,45);
+    M5.Lcd.print("O");
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(32,52);
+    M5.Lcd.print("2");
+
+    if(96 <= x){
+        M5.Lcd.setTextColor(goodColor);
+    }else{
+        M5.Lcd.setTextColor(mainColor);
+    }
+    M5.Lcd.setTextSize(3);
+    M5.Lcd.setCursor(70,45);
+    M5.Lcd.printf("%3d", x);
+}
+
+void setup()
+{
+    Serial.begin(115200);
+    M5.begin();
+    M5.Lcd.setRotation(1);
+    M5.Lcd.fillScreen(WHITE);
+    delay(1000);
+    M5.Lcd.fillScreen(BLACK);
+    mainColor = M5.Lcd.color565(245, 245, 245); // white smoke
+    goodColor = M5.Lcd.color565(0, 250, 154); // medium spring green
+    M5.Lcd.drawXBitmap(10, 5, hb1_bmp, hb1_bmp_width, hb1_bmp_height, mainColor);
+    printHeartBeat(0.0);
+    printSpO2(0);
+    
+    // Initialize the PulseOximeter instance and register a beat-detected callback
+    // The parameter passed to the begin() method changes the samples flow that
+    // the library spews to the serial.
+    // Options:
+    //  * PULSEOXIMETER_DEBUGGINGMODE_PULSEDETECT : filtered samples and beat detection threshold
+    //  * PULSEOXIMETER_DEBUGGINGMODE_RAW_VALUES : sampled values coming from the sensor, with no processing
+    //  * PULSEOXIMETER_DEBUGGINGMODE_AC_VALUES : sampled values after the DC removal filter
+
+    // Initialize the PulseOximeter instance
+    // Failures are generally due to an improper I2C wiring, missing power supply
+    // or wrong target chip
+    if (!pox.begin(PULSEOXIMETER_DEBUGGINGMODE_PULSEDETECT)) {
+        Serial.println("ERROR: Failed to initialize pulse oximeter");
+        for(;;);
+    }
+
+    pox.setOnBeatDetectedCallback(onBeatDetected);
+}
+
+void loop()
+{
+    // Make sure to call update as fast as possible
+    pox.update();
+
+    // Asynchronously dump heart rate and oxidation levels to the serial
+    // For both, a value of 0 means "invalid"
+    if (millis() - tsLastReport > REPORTING_PERIOD_MS) {
+        Serial.print("H:");
+        Serial.println(pox.getHeartRate());
+
+        Serial.print("O:");
+        Serial.println(pox.getSpO2());
+
+        tsLastReport = millis();
+        if(isHeatBeatAvailable){
+            lastBeat++;
+            if(lastBeat > 3){
+                isHeatBeatAvailable = false;
+                lastBeat = 0;
+                M5.Lcd.fillScreen(BLACK);
+                M5.Lcd.drawXBitmap(10, 5, hb1_bmp, hb1_bmp_width, hb1_bmp_height, mainColor);
+            }
+        }
+        printHeartBeat(pox.getHeartRate());
+        printSpO2(pox.getSpO2());
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # M5StickC_HEATRATE_UNIT
+Arduino sketch for M5StickC and M5Stack Heatrate unit.
+
+![device image](https://github.com/kyoto-bt-lab/M5StickC_HEATRATE_UNIT/blob/master/img/sample.jpg)
+
+# Usage
+This library supports the following devices :
+* [M5StickC](https://m5stack.com/products/stick-c)
+* [M5Stack Heatrate unit (MAX30100)](https://m5stack.com/products/mini-heart-unit)
+ 
+You can easily get heart rate and oxygen saturation.  
+Just launch [M5StickC_HEATRATE_UNIT.ino](https://github.com/kyoto-bt-lab/M5StickC_HEATRATE_UNIT/blob/master/M5StickC_HEATRATE_UNIT.ino) and download to device.  
+Press power switch button for two seconds to turn it on, and press and hold for six seconds to turn it off.
